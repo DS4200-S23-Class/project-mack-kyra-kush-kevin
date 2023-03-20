c97ede19d90eb79e7899a37f3d4ec1baf6fd51b2
mmorgan1247 <mack.morgan@gmail.com>
2023-03-20 19:42:18 -0400

    d3

--- js/main.js
+++ js/main.js
@@ -1,27 +1,7 @@
-import {us, states, statemesh, Choropleth, Legend} from "@d3/choropleth"
-
-
-function UsStateChoropleth(data, {
-	features = states,
-	border = statemesh,
-	width = 8975,
-	height = 610,
-	...options
-} = {}) {
-	return Choropleth(data, {features, borders, width, height, ...options});
-}
-
-chartData = FileAttachment("table01a.xlsx").csv({typed: true})
-
-
-chart = UsStateChoropleth(chartData, {
-  id: d => namemap.get(d.name),
-  value: d => d.rate,
-  scale: d3.scaleQuantize,
-  domain: [1, 7],
-  range: d3.schemeBlues[6],
-  title: (f, d) => `${f.properties.name}\n${d?.rate}%`
-})
-
-
-UsStateChoropleth(data, options);
+var map = d3.choropleth()
+    .geofile('/d3-geomap/topojson/countries/USA.json')
+    .projection(d3.geoAlbersUsa)
+    .column('2012')
+    .unitId('fips')
+    .scale(1000)
+    .legend(true);
