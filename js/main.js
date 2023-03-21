70de408e95fe1a6a197d3641a27f98e2c028c3bc
mmorgan1247 <mack.morgan@gmail.com>
2023-03-21 15:19:09 -0400

    map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,63 @@
-var map = d3.choropleth()
-    .geofile('/d3-geomap/topojson/countries/USA.json')
-    .projection(d3.geoAlbersUsa)
-    .column('2012')
-    .unitId('fips')
-    .scale(1000)
-    .legend(true);
+const USA_FRAME = d3.select("#map")
+.append("svg")
+.attr("height", 400)
+.attr("width", 400)
+.attr("class", "usa_frame");
+
+// var map = d3.geomap()
+//     .geofile('/d3-geomap/topojson/world/countries.json')
+//     .draw(d3.select('#map'));
+
+// d3.csv("data_clean/pesticide_consumption.xslx").then((data) => {
+//     console.log(data);
+// });
+
+d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv', function(err, rows){
+                    
+    function unpack(rows, key) {
+        rows = Array.from(rows);
+        return rows.map(function(row) { return row[key]; });
+    }
+
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'USA-states',
+        locations: unpack(rows, 'Postal'),
+        z: unpack(rows, 'Population'),
+        text: unpack(rows, 'State'),
+        autocolorscale: true
+    }];
+
+    var layout = {
+    title: '2014 US Population by State',
+        geo:{
+            scope: 'usa',
+            countrycolor: 'rgb(255, 255, 255)',
+            showland: true,
+            landcolor: 'rgb(217, 217, 217)',
+            showlakes: true,
+            lakecolor: 'rgb(255, 255, 255)',
+            subunitcolor: 'rgb(255, 255, 255)',
+            //showsubunits: true,
+            lonaxis: {},
+            lataxis: {}
+        }
+    };
+    Plotly.newPlot(document.getElementById("map"), data, layout, {showLink: false});
+});
+
+const TOOLTIP = d3.select("#map") 
+            .append("div") 
+            .attr("class", "tooltip") 
+            .style("opacity", 0); 
+
+function handleHover(event, d){
+    event.target.style.fill = "yellow";
+    TOOLTIP.style("opacity", 1);
+};
+
+function handleLeave(event, d){
+    event.target.style.fill = "blueviolet";
+    TOOLTIP.style("opacity", 0);
+};
+
