1349d610f857187c51edf9e96966075d90fe9b8d
mmorgan1247 <mack.morgan@gmail.com>
2023-03-27 15:24:11 -0400

    viz2

--- js/main.js
+++ js/main.js
@@ -64,10 +64,12 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
 
 
-  function build_pesticide(){
-  let prev_point = [0, 0];
+  function build_pesticide(states){
+  states = Array.from(states);
+  
+  //console.log(states)
 
-  d3.csv("data_clean/pesticide_consumption.csv").then((data) => {
+  d3.csv("data_clean/pesticide_consumption_F.csv").then((data) => {
 
     //Add X axis --> it is a date format
     let x = d3.scaleLinear()
@@ -80,28 +82,36 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
     // Add Y axis
     let y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.AL; })])
+      .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
     pest_svg.append("g")
       .call(d3.axisLeft(y));
     // Add the line
 
-    for (let i = 0; i < data.length; i++) {
-      pest_svg.append("line")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr('x1', prev_point[0])
-      .attr('y1', prev_point[1])
-      .attr('x2', x(data[i].Year))
-      .attr('y2', y(data[i].AL));
+    states.forEach(item => {
+      let prev_point = [0, 0];
 
-      prev_point = [x(data[i].Year), y(data[i].AL)];
+      row = data.filter(function(d){ return d.State == item; });
 
 
-    }
 
+      for (let i = 0; i < 45; i++) {
+        pest_svg.append("line")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr('x1', prev_point[0])
+        .attr('y1', prev_point[1])
+        .attr('x2', x(row[i].Year))
+        .attr('y2', y(row[i].Value));
+
+      prev_point = [x(row[i].Year), y(row[i].Value)];
+    };
+
+
+    })
+    
     pest_svg.append("text")
     .attr("x", FRAME_WIDTH/2)
     .attr("y", 0 - MARGINS.top/2)
@@ -109,13 +119,15 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
     .text("Pesticide Consumption Per Year");
 
 
-      fitter(data);
-    })
-  }
-  function build_fertilizer(){
-  let prev_point = [0, 0];
+    
+  })
+}
+  function build_fertilizer(states){
+  states = Array.from(states);
+  
+  //console.log(states)
 
-  d3.csv("data_clean/fertilizer_consumption.csv").then((data) => {
+  d3.csv("data_clean/fertilizer_consumption_F.csv").then((data) => {
 
     //Add X axis --> it is a date format
     let x = d3.scaleLinear()
@@ -128,43 +140,51 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
     // Add Y axis
     let y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.AL; })])
+      .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
     fert_svg.append("g")
       .call(d3.axisLeft(y));
     // Add the line
 
-    for (let i = 0; i < data.length; i++) {
-      fert_svg.append("line")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr('x1', prev_point[0])
-      .attr('y1', prev_point[1])
-      .attr('x2', x(data[i].Year))
-      .attr('y2', y(data[i].AL));
+    states.forEach(item => {
+      let prev_point = [0, 0];
 
-      prev_point = [x(data[i].Year), y(data[i].AL)];
+      row = data.filter(function(d){ return d.State == item; });
+      console.log(row[1].Year);
 
 
-    }
+
+      for (let i = 0; i < 45; i++) {
+        fert_svg.append("line")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr('x1', prev_point[0])
+        .attr('y1', prev_point[1])
+        .attr('x2', x(row[i].Year))
+        .attr('y2', y(row[i].Value));
+
+      prev_point = [x(row[i].Year), y(row[i].Value)];
+    };
+
+    })
     fert_svg.append("text")
       .attr("x", FRAME_WIDTH/2)
       .attr("y", 0 - MARGINS.top/2)
       .attr("text-anchor", "middle") 
       .text("Fertilizer Consumption Per Year");
 
+    
+  })
+}
 
-      fitter(data);
-    })
-  }
+  function build_energy_input(states){
+    states = Array.from(states);
+  
+  //console.log(states)
 
-  function build_energy_input(){
-  let prev_point = [0, 0];
-  let counter = 0;
-  d3.csv("data_clean/energy_input.csv").then((data) => {
-
+    d3.csv("data_clean/energy_input_F.csv").then((data) => {
 
     //Add X axis --> it is a date format
     let x = d3.scaleLinear()
@@ -177,27 +197,34 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
     // Add Y axis
     let y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.AL; })])
+      .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
     energy_svg.append("g")
       .call(d3.axisLeft(y));
     // Add the line
 
-    for (let i = 0; i < data.length; i++) {
-      energy_svg.append("line")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr('x1', prev_point[0])
-      .attr('y1', prev_point[1])
-      .attr('x2', x(data[i].Year))
-      .attr('y2', y(data[i].AL));
+    states.forEach(item => {
+      let prev_point = [0, 0];
 
-      prev_point = [x(data[i].Year), y(data[i].AL)];
+      row = data.filter(function(d){ return d.State == item; });
+      console.log(row[1].Year);
 
 
-    }
+
+      for (let i = 0; i < 45; i++) {
+        energy_svg.append("line")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr('x1', prev_point[0])
+        .attr('y1', prev_point[1])
+        .attr('x2', x(row[i].Year))
+        .attr('y2', y(row[i].Value));
+
+      prev_point = [x(row[i].Year), y(row[i].Value)];
+    };
+    })
 
     
     energy_svg.append("text")
@@ -208,13 +235,18 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
 
   
-    })
+    
+  })
   }
 
-  function build_labor_input(){
-  let prev_point = [0, 0];
+  function build_labor_input(states){
+    d3.selectAll("line").remove();
 
-  d3.csv("data_clean/labour_input.csv").then((data) => {
+  states = Array.from(states);
+  
+  //console.log(states)
+
+  d3.csv("data_clean/labour_input_F.csv").then((data) => {
 
     //Add X axis --> it is a date format
     let x = d3.scaleLinear()
@@ -227,27 +259,38 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
 
     // Add Y axis
     let y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.AL; })])
+      .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
     labor_svg.append("g")
       .call(d3.axisLeft(y));
     // Add the line
 
-    for (let i = 0; i < data.length; i++) {
-      labor_svg.append("line")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr('x1', prev_point[0])
-      .attr('y1', prev_point[1])
-      .attr('x2', x(data[i].Year))
-      .attr('y2', y(data[i].AL));
+    states.forEach(item => {
+      let prev_point = [0, 0];
 
-      prev_point = [x(data[i].Year), y(data[i].AL)];
+      row = data.filter(function(d){ return d.State == item; });
+      console.log(row[1].Year);
 
 
-    }
+
+      for (let i = 0; i < 45; i++) {
+        labor_svg.append("line")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr('x1', prev_point[0])
+        .attr('y1', prev_point[1])
+        .attr('x2', x(row[i].Year))
+        .attr('y2', y(row[i].Value));
+
+      prev_point = [x(row[i].Year), y(row[i].Value)];
+    };
+
+    
+
+
+    })
     labor_svg.append("text")
     .attr("x", FRAME_WIDTH/2)
     .attr("y", 0 - MARGINS.top/2)
@@ -257,8 +300,47 @@ files=["data_clean/labour_input.csv", "data_clean/pesticide_consumption.csv",
     })
   }
 
-  build_pesticide();
-  build_fertilizer();
-  build_energy_input();
-  build_labor_input();
+  let states_selected = [];
+
+  function newLine(){
+    let newState = document.getElementById("selector").value;
+    if (!states_selected.includes(newState)){
+      states_selected.push(newState);
+    }
+    build_pesticide(states_selected);
+    build_fertilizer(states_selected);
+    build_energy_input(states_selected);
+    build_labor_input(states_selected);
+  }
+
+  function removeLine(){
+    let newState = document.getElementById("selector").value;
+
+    if (states_selected.includes(newState)){
+      const index = states_selected.indexOf(newState);
+      states_selected.splice(index, 1);
+
+      console.log(states_selected);
+
+      build_pesticide(states_selected);
+      build_fertilizer(states_selected);
+      build_energy_input(states_selected);
+      build_labor_input(states_selected);
+    }
+
+    
+    
+    //console.log(states_selected);
+  }
+
+  // selecting states works. find a way to turn the strings into function calls
+  // so that the below functions take in a set of strings (selected states)
+  // and plots their data. 
+
+build_pesticide([]);
+build_fertilizer([]);
+build_energy_input([]);
+build_labor_input([]);
+
+
 
