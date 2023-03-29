7f22e83de8a32bc31e00443b7992c2ea7343fa0e
kqueally <queally.k@northeastern.edu>
2023-03-29 15:23:48 -0400

    ahhhhh

--- js/main.js
+++ js/main.js
@@ -8,106 +8,184 @@ const VIS_WIDTH_LONG = FRAME_WIDTH_LONG - MARGINS.left - MARGINS.right;
 
 /**************************************************************/
 /**************************************************************/
-// append the svg object to the body of the page
-let svg = d3.select("#viz1")
-  .append("svg")
-    .attr("width", FRAME_WIDTH_LONG)
-    .attr("height", FRAME_HEIGHT)
-  .append("g")
-    .attr("transform",
-          "translate(" + MARGINS.left + "," + MARGINS.top + ")");
+function build_outputs() {
 
-// List of groups (here I have one group per column)
-let allGroup = ["Total Output", "All Livestock and Products",
-  "Livestock and Products: Meat", "Livestock and Products: Dairy",
-  "Livestock and Products: Poultry and Eggs", "All Crops", "Crops: Food Grains",
-  "Crops: Feed Crops", "Crops: Oil Crops", "Crops: Vegetables and Melons",
-  "Crops: Fruits and Tree Nuts", "Crops: Other"];
+  // append the svg object to the body of the page
+  let svg = d3.select("#viz1")
+    .append("svg")
+      .attr("width", FRAME_WIDTH_LONG)
+      .attr("height", FRAME_HEIGHT)
+    .append("g")
+      .attr("transform",
+            "translate(" + MARGINS.left + "," + MARGINS.top + ")");
 
-// add the options to the button
-d3.select("#selectButton")
-  .selectAll('myOptions')
- 	.data(allGroup)
-  .enter()
-  	.append('option')
-    .text(d => d)
-    .attr("value", d => d);
+  // List of groups (here I have one group per column)
+  let allGroup = ["Total Output", "All Livestock and Products",
+    "Livestock and Products: Meat", "Livestock and Products: Dairy",
+    "Livestock and Products: Poultry and Eggs", "All Crops", "Crops: Food Grains",
+    "Crops: Feed Crops", "Crops: Oil Crops", "Crops: Vegetables and Melons",
+    "Crops: Fruits and Tree Nuts", "Crops: Other"];
 
-// Add X axis --> it is a date format
-let x = d3.scaleLinear()
-  .domain([1960,2004])
-  .range([ 0, VIS_WIDTH_LONG ]);
-svg.append("g")
-  .attr("transform", "translate(0," + VIS_HEIGHT + ")")
-  .call(d3.axisBottom(x));
+  // add the options to the button
+  d3.select("#selectButton")
+    .selectAll('myOptions')
+   	.data(allGroup)
+    .enter()
+    	.append('option')
+      .text(d => d)
+      .attr("value", d => d);
 
-// Add Y axis
-let y = d3.scaleLinear()
-  .domain( [0,425000])
-  .range([ VIS_HEIGHT, 0 ]);
-svg.append("g")
-  .call(d3.axisLeft(y));
+  // Add X axis --> it is a date format
+  let x = d3.scaleLinear()
+    .domain([1960,2004])
+    .range([ 0, VIS_WIDTH_LONG ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + VIS_HEIGHT + ")")
+    .call(d3.axisBottom(x));
 
-//Read the data
-d3.csv("data_clean/table01a.csv", function(data) {
+  // Add Y axis
+  let y = d3.scaleLinear()
+    .domain( [0,425000])
+    .range([ VIS_HEIGHT, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-    // Initialize line with group a
-    let line = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(d.Year); })
-          .y(function(d) { return y(d["Total Output"]); })
-        )
-        .attr("stroke", "black")
-        .style("stroke-width", 4)
-        .style("fill", "none");
+  //Read the data
+  d3.csv("data_clean/table01a_F.csv").then(function(data) {
+  /*
+    let prev_point = [0, y(data[0]["Total.Output"])];
 
-    // Initialize dots with group a
-    let dot = svg
-      .selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-        .attr("cx", function(d) { return x(1970) })
-        .attr("cy", function(d) { return y(100000) })
-        .attr("r", 7)
-        .style("fill", "#69b3a2")
+    for (let i = 0; i < 45; i++) {
+      pest_svg.append("line")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("stroke-width", 1.5)
+      .attr('x1', prev_point[0])
+      .attr('y1', prev_point[1])
+      .attr('x2', x(row[i].Year))
+      .attr('y2', y(row[i]["Total.Output"]));
 
+      prev_point = [x(row[i].Year), y(row[i].Value)];
+    }*/
 
-    // A function that update the chart
-    function update(selectedGroup) {
+      // Initialize line with group a
+      let line = svg
+        .append('g')
+        .append("path")
+          .datum(data)
+          .attr("d", d3.line()
+            .x(function(d) { console.log(d["Year"]); return x(parseInt(d["Year"])); })
+            .y(function(d) { console.log(d["Total Output"]); return y(parseInt(d["Total Output"])); })
+          )
+          .attr("stroke", "black")
+          .style("stroke-width", 4)
+          .style("fill", "none");
 
-      // Create new data with the selection?
-      const dataFilter = data.map(function(d){return {Year: d.Year, value:d[selectedGroup]} })
+      // Initialize dots with group a
+      let dot = svg
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+          .attr("cx", function(d) { return x(parseInt(d["Year"])); })
+          .attr("cy", function(d) { return y(parseInt(d["Total Output"])); })
+          .attr("r", 7)
+          .style("fill", "#69b3a2")
 
-      // Give these new data to update line
-      line
-          .datum(dataFilter)
+/*
+      // A function that update the chart
+      function update(selectedGroup) {
+
+        // Create new data with the selection?
+        const dataFilter = data.map(function(d){return {Year: d.Year, value:d[selectedGroup]} })
+
+        // Give these new data to update line
+        line
+            .datum(dataFilter)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+              .x(function(d) { return x(+d.Year) })
+              .y(function(d) { return y(+d.value) })
+            )
+        dot
+          .data(dataFilter)
           .transition()
           .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d.Year) })
-            .y(function(d) { return y(+d.value) })
-          )
-      dot
-        .data(dataFilter)
-        .transition()
-        .duration(1000)
-          .attr("cx", function(d) { return x(+d.Year) })
-          .attr("cy", function(d) { return y(+d.value) })
-    }
+            .attr("cx", function(d) { return x(+d.Year) })
+            .attr("cy", function(d) { return y(+d.value) })
+      }
 
-    // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) {
-        // recover the option that has been chosen
-        let selectedOption = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(selectedOption)
-    })
+      // When the button is changed, run the updateChart function
+      d3.select("#selectButton").on("change", function(d) {
+          // recover the option that has been chosen
+          let selectedOption = d3.select(this).property("value")
+          // run the updateChart function with this selected option
+          update(selectedOption)
+      })*/
+
+  });
+};
+
+function build_stacked_outputs() {
+
+  let svg = d3.select("#viz1")
+    .append("svg")
+      .attr("width", FRAME_WIDTH_LONG)
+      .attr("height", FRAME_HEIGHT)
+    .append("g")
+      .attr("transform",
+            "translate(" + MARGINS.left + "," + MARGINS.top + ")");
+
+// Parse the Data
+d3.csv("data_clean/table01a_F.csv").then(function(data) {
+
+  // List of groups = header of the csv files
+  let keys = data.columns.slice(1)
+
+  // Add X axis
+  let x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) { return d.year; }))
+    .range([ 0, VIS_WIDTH_LONG ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + VIS_HEIGHT + ")")
+    .call(d3.axisBottom(x).ticks(5));
+
+  // Add Y axis
+  let y = d3.scaleLinear()
+    .domain([0, 450000])
+    .range([ VIS_HEIGHT, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // color palette
+  let color = d3.scaleOrdinal()
+    .domain(keys)
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#f781bf','#f781bf','#f781bf','#f781bf'])
+
+  //stack the data?
+  let stackedData = d3.stack()
+    .keys(keys)
+    (data)
+    //console.log("This is the stack result: ", stackedData)
+
+  // Show the areas
+  svg
+    .selectAll("mylayers")
+    .data(stackedData)
+    .enter()
+    .append("path")
+      .style("fill", function(d) { console.log(d.key) ; return color(d.key); })
+      .attr("d", d3.area()
+        .x(function(d, i) { return x(d.data.year); })
+        .y0(function(d) { return y(d[0]); })
+        .y1(function(d) { return y(d[1]); })
+    )
 
 })
+}
+
 
 
 const colors = ['steelblue', 'violet', 'forestgreen', 'darkred', 'grey', 'black', 'brown', 'orange'];
@@ -137,13 +215,13 @@ let energy_svg = d3.select("#energy")
     .attr("transform",
           "translate(" + MARGINS.left + "," + MARGINS.top + ")");
 let labor_svg = d3.select("#labor")
- 
+
   .append("svg")
     .attr("width", FRAME_WIDTH + MARGINS.left + MARGINS.right)
     .attr("height", FRAME_HEIGHT + MARGINS.top + MARGINS.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + MARGINS.left + "," + MARGINS.top + ")"); 
+          "translate(" + MARGINS.left + "," + MARGINS.top + ")");
 let labor_tooltip = d3.select("#labor")
             .append("div")
             .style("position", "absolute")
@@ -187,7 +265,7 @@ let pest_tooltip = d3.select("#pest")
 
 
 function build_pesticide(states, firstRun){
-  
+
   states = Array.from(states);
 
   d3.csv("data_clean/pesticide_consumption_F.csv").then((data) => {
@@ -205,7 +283,7 @@ function build_pesticide(states, firstRun){
       pest_svg.append("text")
             .attr("x", FRAME_WIDTH/2)
             .attr("y", 0 - MARGINS.top/2)
-            .attr("text-anchor", "middle") 
+            .attr("text-anchor", "middle")
             .text("Pesticide Consumption Per Year")
       pest_svg.append("text")
             .attr("class", "x label")
@@ -230,15 +308,15 @@ function build_pesticide(states, firstRun){
       pest_svg.append("text")
         .attr("x", FRAME_WIDTH/2)
         .attr("y", 0 - MARGINS.top/2)
-        .attr("text-anchor", "middle") 
+        .attr("text-anchor", "middle")
         .text("Pesticide Consumption Per Year");
 
     }
-    
 
-    
-    
-    
+
+
+
+
     // Add the line
     let state_count = 0;
 
@@ -285,24 +363,24 @@ function build_pesticide(states, firstRun){
       state_count ++;
     })
 
-    
 
 
-    
+
+
   })
 
-    
+
 }
   function build_fertilizer(states, firstRun){
   states = Array.from(states);
-  
+
       d3.csv("data_clean/fertilizer_consumption_F.csv").then((data) => {
 
     let x = d3.scaleLinear()
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
       .range([ 0, FRAME_WIDTH ]);
-    
+
 
     // Add Y axis
     let y = d3.scaleLinear()
@@ -318,7 +396,7 @@ function build_pesticide(states, firstRun){
     fert_svg.append("text")
           .attr("x", FRAME_WIDTH/2)
           .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
+          .attr("text-anchor", "middle")
           .text("Fertilizer Consumption Per Year")
     fert_svg.append("text")
           .attr("class", "y label")
@@ -382,21 +460,21 @@ function build_pesticide(states, firstRun){
       state_count ++;
     })
 
-    
+
   })
 }
 
   function build_energy_input(states, firstRun){
     states = Array.from(states);
-  
-    
+
+
     d3.csv("data_clean/energy_input_F.csv").then((data) => {
 
     let x = d3.scaleLinear()
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
       .range([ 0, FRAME_WIDTH ]);
-    
+
 
     // Add Y axis
     let y = d3.scaleLinear()
@@ -407,7 +485,7 @@ function build_pesticide(states, firstRun){
         energy_svg.append("text")
           .attr("x", FRAME_WIDTH/2)
           .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
+          .attr("text-anchor", "middle")
           .text("Energy Input Per Year")
         energy_svg.append("text")
             .attr("class", "y label")
@@ -429,7 +507,7 @@ function build_pesticide(states, firstRun){
           .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
           .call(d3.axisBottom(x));
       }
-    
+
     // Add the line
       let state_count = 0;
 
@@ -477,8 +555,8 @@ function build_pesticide(states, firstRun){
 
 
 
-  
-    
+
+
   })
   }
 
@@ -486,17 +564,17 @@ function build_pesticide(states, firstRun){
     d3.selectAll("line").remove();
 
     states = Array.from(states);
-  
-    
+
+
   d3.csv("data_clean/labour_input_F.csv").then((data) => {
 
 
-  
+
     let x = d3.scaleLinear()
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
       .range([ 0, FRAME_WIDTH ]);
-    
+
 
     // Add Y axis
     let y = d3.scaleLinear()
@@ -512,7 +590,7 @@ function build_pesticide(states, firstRun){
       labor_svg.append("text")
           .attr("x", FRAME_WIDTH/2)
           .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
+          .attr("text-anchor", "middle")
           .text("Labor Input Per Year")
       labor_svg.append("text")
             .attr("class", "y label")
@@ -529,14 +607,14 @@ function build_pesticide(states, firstRun){
           .attr("y", FRAME_HEIGHT + 40)
           .text("Year");
     }
-    
+
     let state_count = 0;
 
     states.forEach(item => {
       let prev_point = [0, y(data[0].Value)];
       row = data.filter(function(d){ return d.State == item; });
 
-      
+
 
       for (let i = 0; i < 45; i++) {
         labor_svg.append("line")
@@ -574,16 +652,16 @@ function build_pesticide(states, firstRun){
 
         prev_point = [x(row[i].Year), y(row[i].Value)];
 
-        
-        
 
-        
 
-        
+
+
+
+
     };
       state_count++;
     })
-    
+
 
     })
   }
@@ -607,7 +685,7 @@ function build_pesticide(states, firstRun){
 
 
 
-    
+
 
   }
 
@@ -629,7 +707,7 @@ function build_pesticide(states, firstRun){
 
       dispKey();
     }
-  
+
     //console.log(states_selected);
   }
 
@@ -648,9 +726,10 @@ function build_pesticide(states, firstRun){
 
   // selecting states works. find a way to turn the strings into function calls
   // so that the below functions take in a set of strings (selected states)
-  // and plots their data. 
+  // and plots their data.
 
 build_pesticide([], true);
 build_fertilizer([], true);
 build_energy_input([], true);
 build_labor_input([], true);
+build_outputs();
