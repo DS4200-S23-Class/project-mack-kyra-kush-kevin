7dae9d1bd20fb21211d5518797d1247c964c4a5b
mmorgan1247 <mack.morgan@gmail.com>
2023-03-28 23:43:32 -0400

    viz2 99percent

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
-
+const colors = ['steelblue', 'violet', 'forestgreen', 'darkred'];
 
 let pest_svg = d3.select("#pest")
   .append("svg")
@@ -16,7 +16,6 @@ let pest_svg = d3.select("#pest")
   .append("g")
     .attr("transform",
           "translate(" + MARGINS.left + "," + MARGINS.top + ")");
-
 let fert_svg = d3.select("#fert")
   .append("svg")
     .attr("width", FRAME_WIDTH + MARGINS.left + MARGINS.right)
@@ -24,7 +23,6 @@ let fert_svg = d3.select("#fert")
   .append("g")
     .attr("transform",
           "translate("  + MARGINS.left + "," + MARGINS.top + ")");
-
 let energy_svg = d3.select("#energy")
   .append("svg")
     .attr("width", FRAME_WIDTH + MARGINS.left + MARGINS.right)
@@ -39,8 +37,7 @@ let labor_svg = d3.select("#labor")
     .attr("height", FRAME_HEIGHT + MARGINS.top + MARGINS.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + MARGINS.left + "," + MARGINS.top + ")");
-  
+          "translate(" + MARGINS.left + "," + MARGINS.top + ")"); 
 let labor_tooltip = d3.select("#labor")
             .append("div")
             .style("position", "absolute")
@@ -51,7 +48,6 @@ let labor_tooltip = d3.select("#labor")
             .style("border-width", "2px")
             .style("border-radius", "5px")
             .style("padding", "5px");
-
 let energy_tooltip = d3.select("#energy")
             .append("div")
             .style("position", "absolute")
@@ -62,7 +58,6 @@ let energy_tooltip = d3.select("#energy")
             .style("border-width", "2px")
             .style("border-radius", "5px")
             .style("padding", "5px");
-
 let fert_tooltip = d3.select("#fert")
             .append("div")
             .style("position", "absolute")
@@ -73,7 +68,6 @@ let fert_tooltip = d3.select("#fert")
             .style("border-width", "2px")
             .style("border-radius", "5px")
             .style("padding", "5px");
-
 let pest_tooltip = d3.select("#pest")
             .append("div")
             .style("position", "absolute")
@@ -86,20 +80,11 @@ let pest_tooltip = d3.select("#pest")
             .style("padding", "5px");
 
 
-
-
-function build_pesticide(states){
+function build_pesticide(states, firstRun){
   
-
   states = Array.from(states);
 
-
-
-    
-    
   d3.csv("data_clean/pesticide_consumption_F.csv").then((data) => {
-
-
     let x = d3.scaleLinear()
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
@@ -109,39 +94,42 @@ function build_pesticide(states){
       .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
 
-
-    pest_svg.append("text")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
-          .text("Pesticide Consumption Per Year")
-    pest_svg.append("text")
-          .attr("class", "x label")
-          .attr("text-anchor", "end")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", FRAME_HEIGHT + 40)
-          .text("Year");
-    pest_svg.append("g")
-      .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
-      .call(d3.axisBottom(x));
-    pest_svg.append("g")
-      .call(d3.axisLeft(y));
+    if(firstRun) {
 
       pest_svg.append("text")
-      .attr("x", FRAME_WIDTH/2)
-      .attr("y", 0 - MARGINS.top/2)
-      .attr("text-anchor", "middle") 
-      .text("Pesticide Consumption Per Year");
+            .attr("x", FRAME_WIDTH/2)
+            .attr("y", 0 - MARGINS.top/2)
+            .attr("text-anchor", "middle") 
+            .text("Pesticide Consumption Per Year")
+      pest_svg.append("text")
+            .attr("class", "x label")
+            .attr("text-anchor", "end")
+            .attr("x", FRAME_WIDTH/2)
+            .attr("y", FRAME_HEIGHT + 40)
+            .text("Year");
+      pest_svg.append("g")
+        .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
+        .call(d3.axisBottom(x));
+      pest_svg.append("g")
+        .call(d3.axisLeft(y));
 
-      firstRun = 0;
+      pest_svg.append("text")
+        .attr("x", FRAME_WIDTH/2)
+        .attr("y", 0 - MARGINS.top/2)
+        .attr("text-anchor", "middle") 
+        .text("Pesticide Consumption Per Year");
+
+    }
+    
+
     
     
     
     // Add the line
+    let state_count = 0;
 
     states.forEach(item => {
       let prev_point = [0, y(data[0].Value)];
-
       row = data.filter(function(d){ return d.State == item; });
 
 
@@ -150,14 +138,16 @@ function build_pesticide(states){
         pest_svg.append("line")
         .datum(data)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
+        .attr("stroke", colors[state_count])
         .attr("stroke-width", 1.5)
         .attr('x1', prev_point[0])
         .attr('y1', prev_point[1])
         .attr('x2', x(row[i].Year))
         .attr('y2', y(row[i].Value))
         .on("mouseover", function(event, d){
-          d3.select(item).style("opacity", 9);
+          //row = data.filter(function(d) {return d.State == item && d.Year == 1960+i;})
+          d3.select("#" + item).attr("stroke-width", 3);
+
           pest_tooltip
             .style("opacity", 1)
             .html(row[i].State + " in " + row[i].Year +": " + row[i].Value)
@@ -178,7 +168,7 @@ function build_pesticide(states){
       prev_point = [x(row[i].Year), y(row[i].Value)];
     };
 
-
+      state_count ++;
     })
 
     
@@ -189,9 +179,28 @@ function build_pesticide(states){
 
     
 }
-  function build_fertilizer(states){
+  function build_fertilizer(states, firstRun){
   states = Array.from(states);
   
+      d3.csv("data_clean/fertilizer_consumption_F.csv").then((data) => {
+
+    let x = d3.scaleLinear()
+      .domain([d3.min(data, function(d) { return d.Year; })
+        , d3.max(data, function(d) { return d.Year; })])
+      .range([ 0, FRAME_WIDTH ]);
+    
+
+    // Add Y axis
+    let y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.Value; })])
+      .range([ FRAME_HEIGHT, 0 ]);
+
+    if(firstRun) {
+    fert_svg.append("g")
+      .call(d3.axisLeft(y));
+    fert_svg.append("g")
+      .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
+      .call(d3.axisBottom(x));
     fert_svg.append("text")
           .attr("x", FRAME_WIDTH/2)
           .attr("y", 0 - MARGINS.top/2)
@@ -203,24 +212,12 @@ function build_pesticide(states){
           .attr("x", FRAME_WIDTH/2)
           .attr("y", FRAME_HEIGHT + 40)
           .text("Year");
-  d3.csv("data_clean/fertilizer_consumption_F.csv").then((data) => {
+    }
 
-    let x = d3.scaleLinear()
-      .domain([d3.min(data, function(d) { return d.Year; })
-        , d3.max(data, function(d) { return d.Year; })])
-      .range([ 0, FRAME_WIDTH ]);
-    fert_svg.append("g")
-      .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
-      .call(d3.axisBottom(x));
 
-    // Add Y axis
-    let y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.Value; })])
-      .range([ FRAME_HEIGHT, 0 ]);
-    fert_svg.append("g")
-      .call(d3.axisLeft(y));
     // Add the line
 
+    let state_count = 0;
     states.forEach(item => {
       let prev_point = [0, y(data[0].Value)];
 
@@ -233,7 +230,7 @@ function build_pesticide(states){
         fert_svg.append("line")
         .datum(data)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
+        .attr("stroke", colors[state_count])
         .attr("stroke-width", 1.5)
         .attr('x1', prev_point[0])
         .attr('y1', prev_point[1])
@@ -260,53 +257,54 @@ function build_pesticide(states){
 
       prev_point = [x(row[i].Year), y(row[i].Value)];
     };
-
+      state_count ++;
     })
-    fert_svg.append("text")
-      .attr("x", FRAME_WIDTH/2)
-      .attr("y", 0 - MARGINS.top/2)
-      .attr("text-anchor", "middle") 
-      .text("Fertilizer Consumption Per Year");
 
     
   })
 }
 
-  function build_energy_input(states){
+  function build_energy_input(states, firstRun){
     states = Array.from(states);
   
-    energy_svg.append("text")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
-          .text("Energy Input Per Year")
-    energy_svg.append("text")
-          .attr("class", "x label")
-          .attr("text-anchor", "end")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", FRAME_HEIGHT + 40)
-          .text("Year");
+    
     d3.csv("data_clean/energy_input_F.csv").then((data) => {
 
     let x = d3.scaleLinear()
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
       .range([ 0, FRAME_WIDTH ]);
-    energy_svg.append("g")
-      .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
-      .call(d3.axisBottom(x));
+    
 
     // Add Y axis
     let y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
-    energy_svg.append("g")
-      .call(d3.axisLeft(y));
+
+    if(firstRun) {
+        energy_svg.append("text")
+          .attr("x", FRAME_WIDTH/2)
+          .attr("y", 0 - MARGINS.top/2)
+          .attr("text-anchor", "middle") 
+          .text("Energy Input Per Year")
+        energy_svg.append("text")
+          .attr("class", "x label")
+          .attr("text-anchor", "end")
+          .attr("x", FRAME_WIDTH/2)
+          .attr("y", FRAME_HEIGHT + 40)
+          .text("Year");
+        energy_svg.append("g")
+          .call(d3.axisLeft(y));
+        energy_svg.append("g")
+          .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
+          .call(d3.axisBottom(x));
+      }
+    
     // Add the line
+      let state_count = 0;
 
     states.forEach(item => {
       let prev_point = [0, y(data[0].Value)];
-
       row = data.filter(function(d){ return d.State == item; });
 
 
@@ -315,7 +313,7 @@ function build_pesticide(states){
         energy_svg.append("line")
         .datum(data)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
+        .attr("stroke", colors[state_count])
         .attr("stroke-width", 1.5)
         .attr('x1', prev_point[0])
         .attr('y1', prev_point[1])
@@ -342,6 +340,9 @@ function build_pesticide(states){
 
       prev_point = [x(row[i].Year), y(row[i].Value)];
     };
+
+      state_count++;
+
     })
 
 
@@ -351,22 +352,12 @@ function build_pesticide(states){
   })
   }
 
-  function build_labor_input(states){
+  function build_labor_input(states, firstRun){
     d3.selectAll("line").remove();
 
     states = Array.from(states);
   
-    labor_svg.append("text")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", 0 - MARGINS.top/2)
-          .attr("text-anchor", "middle") 
-          .text("Labor Input Per Year")
-    labor_svg.append("text")
-          .attr("class", "x label")
-          .attr("text-anchor", "end")
-          .attr("x", FRAME_WIDTH/2)
-          .attr("y", FRAME_HEIGHT + 40)
-          .text("Year");
+    
   d3.csv("data_clean/labour_input_F.csv").then((data) => {
 
 
@@ -375,20 +366,36 @@ function build_pesticide(states){
       .domain([d3.min(data, function(d) { return d.Year; })
         , d3.max(data, function(d) { return d.Year; })])
       .range([ 0, FRAME_WIDTH ]);
-    labor_svg.append("g")
-      .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
-      .call(d3.axisBottom(x));
+    
 
     // Add Y axis
     let y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d.Value; })])
       .range([ FRAME_HEIGHT, 0 ]);
-    labor_svg.append("g")
-      .call(d3.axisLeft(y));
+
+    if(firstRun) {
+      labor_svg.append("g")
+        .call(d3.axisLeft(y));
+      labor_svg.append("g")
+        .attr("transform", "translate(0," + FRAME_HEIGHT + ")")
+        .call(d3.axisBottom(x));
+      labor_svg.append("text")
+          .attr("x", FRAME_WIDTH/2)
+          .attr("y", 0 - MARGINS.top/2)
+          .attr("text-anchor", "middle") 
+          .text("Labor Input Per Year")
+      labor_svg.append("text")
+          .attr("class", "x label")
+          .attr("text-anchor", "end")
+          .attr("x", FRAME_WIDTH/2)
+          .attr("y", FRAME_HEIGHT + 40)
+          .text("Year");
+    }
+    
+    let state_count = 0;
 
     states.forEach(item => {
       let prev_point = [0, y(data[0].Value)];
-
       row = data.filter(function(d){ return d.State == item; });
 
       
@@ -399,14 +406,14 @@ function build_pesticide(states){
         //.enter()
         .attr("id", item)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
+        .attr("stroke", colors[state_count])
         .attr("stroke-width", 1.5)
         .attr('x1', prev_point[0])
         .attr('y1', prev_point[1])
         .attr('x2', x(row[i].Year))
         .attr('y2', y(row[i].Value))
         .on("mouseover", function(event, d){
-          d3.select(item).style("opacity", 9);
+          d3.select(item).attr("stroke-width", 3);
           labor_tooltip
             .style("opacity", 1)
             .html(row[i].State + " in " + row[i].Year +": " + row[i].Value)
@@ -436,7 +443,7 @@ function build_pesticide(states){
 
         
     };
-
+      state_count++;
     })
     
 
@@ -449,19 +456,20 @@ function build_pesticide(states){
     let newState = document.getElementById("selector").value;
     if (!states_selected.includes(newState)){
       states_selected.push(newState);
+      dispKey();
     }
-    build_pesticide(states_selected);
-    build_fertilizer(states_selected);
-    build_energy_input(states_selected);
-    build_labor_input(states_selected);
+    build_pesticide(states_selected, false);
+    build_fertilizer(states_selected, false);
+    build_energy_input(states_selected, false);
+    build_labor_input(states_selected, false);
 
     let key = document.getElementById("list");
 
     d3.selectAll().remove();
 
-    states_selected.forEach(item => {
-      key.innerHTML += "<li id='"+ item + "'>" + item + 'color</li>'
-    })
+
+
+    
 
   }
 
@@ -476,20 +484,35 @@ function build_pesticide(states){
 
       //console.log(states_selected);
 
-      build_pesticide(states_selected);
-      build_fertilizer(states_selected);
-      build_energy_input(states_selected);
-      build_labor_input(states_selected);
+      build_pesticide(states_selected, false);
+      build_fertilizer(states_selected, false);
+      build_energy_input(states_selected, false);
+      build_labor_input(states_selected, false);
+
+      dispKey();
     }
   
     //console.log(states_selected);
+  }
+
+  function dispKey(){
+    let state_count = 0;
+    d3.selectAll("li").remove()
+
+    states_selected.forEach(item => {
+      d3.select("#list")
+        .append("li")
+        .attr("class", "keypoint")
+        .html(item + ": " + colors[state_count]);
+      state_count ++;
+    })
   }
 
   // selecting states works. find a way to turn the strings into function calls
   // so that the below functions take in a set of strings (selected states)
   // and plots their data. 
 
-build_pesticide([]);
-build_fertilizer([]);
-build_energy_input([]);
-build_labor_input([]);
+build_pesticide([], true);
+build_fertilizer([], true);
+build_energy_input([], true);
+build_labor_input([], true);
